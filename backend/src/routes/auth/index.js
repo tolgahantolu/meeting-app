abfb23d70e105e43e8750544020cf314be8e284e
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-08-15 12:13:12 +0300

    backend: validation with joi

--- backend/src/routes/auth/index.js
+++ backend/src/routes/auth/index.js
@@ -1,21 +1,53 @@
 import express from "express";
 import Boom from "@hapi/boom";
+import bcrypt from "bcryptjs";
+import Hasura from "../../clients/hasura";
+import { IS_USER_EXIST, MUTATION_INSERT_USER } from "./queries";
+import { registerSchema } from "./validations";
 
 const router = express.Router();
 
-router.post("/register", (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   const input = req.body.input.data;
 
-  if (!input.email || !input.password) {
-    return next(
-      Boom.badRequest(
-        "Your data is not valid! Please check your email and password then try again."
-      )
-    );
-  }
+  input.email = input.email.toLowerCase();
 
-  console.log("input", input);
-  res.json({ accessToken: "accessToken" });
+  const { error } = registerSchema.validate(input);
+
+  if (error) return next(Boom.badRequest(error.details[0].message));
+
+  //  if (!input.email || !input.password) {
+  //    return next(
+  //      Boom.badRequest(
+  //        "Your data is not valid! Please check your email and password then try again."
+  //      )
+  //    );
+  //  }
+
+  try {
+    const isUserExist = await Hasura.request(IS_USER_EXIST, {
+      email: input.email,
+    });
+
+    if (isUserExist.mt_users.length > 0)
+      throw Boom.conflict(`User already exist! (${input.email})`);
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(input.password, salt);
+
+    const user = await Hasura.request(MUTATION_INSERT_USER, {
+      input: {
+        ...input,
+        password: hash,
+      },
+    });
+
+    console.log(user);
+
+    res.json({ accessToken: "accessToken" });
+  } catch (error) {
+    return next(Boom.badRequest(error));
+  }
 });
 
 export default router;
---
+++ backend/src/routes/auth/queries.js
@@ -0,0 +1,18 @@
+import { gql } from "graphql-request";
+
+export const IS_USER_EXIST = gql`
+  query isEmailExist($email: String!) {
+    mt_users(where: { email: { _eq: $email } }) {
+      id
+    }
+  }
+`;
+
+export const MUTATION_INSERT_USER = gql`
+  mutation insertUser($input: mt_users_insert_input!) {
+    insert_mt_users_one(object: $input) {
+      id
+      name
+    }
+  }
+`;
