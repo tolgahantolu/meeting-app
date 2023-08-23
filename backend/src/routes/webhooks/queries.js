78c09ed3bd92711253fd70fc2c5784eca4b56dba
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-08-23 10:40:09 +0300

    backend: scheduled events - hasura

--- backend/src/routes/webhooks/queries.js
+++ backend/src/routes/webhooks/queries.js
@@ -3,6 +3,7 @@ import { gql } from "graphql-request";
 export const GET_MEETING_PARTICIPANTS = gql`
   query getMeetingParticipants($meeting_id: Int!) {
     mt_participants(where: { meeting_id: { _eq: $meeting_id } }) {
+      meeting_date
       user {
         id
         email
