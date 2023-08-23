8b7f099b93f3203e14a2dab353967c67d4ffd277
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-08-23 10:39:58 +0300

    backend: scheduled events - hasura

--- backend/src/routes/webhooks/index.js
+++ backend/src/routes/webhooks/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import moment from "moment";
+import axios from "axios";
 import Boom from "@hapi/boom";
 import Hasura from "../../clients/hasura";
 import { GET_MEETING_PARTICIPANTS } from "./queries";
@@ -14,7 +16,32 @@ router.post("/meeting_created", async (req, res, next) => {
       meeting_id: meeting.id,
     }
   );
+
+  const scheduleEvent = {
+    type: "create_scheduled_event",
+    args: {
+      webhook: "{{ACTION_BASE_ENDPOINT}}/webhooks/meeting_reminder",
+      schedule_at: moment(participants.meeting_date).subtract(2, "minutes"),
+      payload: {
+        meeting_id: meeting.id,
+      },
+    },
+  };
+
+  const addEvent = await axios("http://localhost:8080/v1/query", {
+    method: "POST",
+    data: JSON.stringify(scheduleEvent),
+    headers: {
+      "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET,
+    },
+  });
+
+  const eventData = addEvent.data;
   console.log(participants);
+});
+
+router.post("/meeting_reminder", async (req, res, next) => {
+  console.log("meeting reminder");
 });
 
 export default router;
